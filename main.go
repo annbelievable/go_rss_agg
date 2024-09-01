8ff86933ca585b992a38615689834b2a257b66f9
kokann <kokann_92@hotmail.com>
2024-09-01 22:29:36 +0800

    First commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	//v1Router.HandleFunc("/health", handlerReadiness)
+	// by doing below, we limit the handler to handle GET method request only
+	v1Router.Get("/health", handlerReadiness)
+	v1Router.Get("/error", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on PORT: %v", portString)
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
---
+++ notes.md
@@ -0,0 +1,20 @@
+https://www.youtube.com/watch?v=un6ZyFkqFKo&list=WL&index=127
+
+29/07/2024 - 2h
+02/08/2024 - 2h30m
+03/08/2024 - 3h22m
+11/08/2024 - 3h47m
+13/08/2024 - 3h47m
+20/08/2024 - 4h06m
+23/08/2024 - 4h49m
+26/08/2024 - 5h30m
+30/08/2024 - 5h30m
+31/08/2024 - 6h03m
+01/09/2024 - 6h43m
+
+linux command for build and running the built binary
+go build && ./<project_name>
+
+go mod vendor
+
+go mod tidy - to tidy up imports
